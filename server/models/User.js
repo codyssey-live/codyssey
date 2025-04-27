afa9765f1d726c0baca3006092e182200bb3c47f
tushrahuja <ahuja.tushar.m@gmail.com>
2025-04-27 20:34:46 +0530

    backend started with user auth

---
+++ server/models/User.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profilePicture: {
+    type: String,
+    default: 'https://example.com/default-profile-picture.png', // Placeholder URL
+  },
+  friendList: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+export const User = mongoose.model('User', userSchema);
